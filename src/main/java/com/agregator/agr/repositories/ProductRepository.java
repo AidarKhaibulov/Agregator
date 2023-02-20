fe64520060f22211e13f927a5a2a013c0a1b92b4
Aidar <myakkaynt228@gmail.com>
2023-02-20 08:26:16 +0300

    added search by categories and appropriate ui buttons

--- src/main/java/com/agregator/agr/repositories/ProductRepository.java
+++ src/main/java/com/agregator/agr/repositories/ProductRepository.java
@@ -11,6 +11,6 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
     List<Product> searchProducts(String query);
 
     //TODO: rewrite sql query in order to find by categories
-@Query("select p from Product p  where p.title like concat('%',:query,'%')")
+@Query("select p from Product p join Category c on c.id = p.category.id where c.name like concat('%',:query,'%')")
     List<Product> searchProductsByCategory(String query);
 }
