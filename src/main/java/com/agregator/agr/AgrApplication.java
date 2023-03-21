61469bb2f8a2b6b616e18f6b8c5cc049686a97e9
Aidar <myakkaynt228@gmail.com>
2023-03-21 12:48:02 +0300

    small fixes env variables

--- src/main/java/com/agregator/agr/AgrApplication.java
+++ src/main/java/com/agregator/agr/AgrApplication.java
@@ -11,19 +11,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AgrApplication {
-	static String DB_USERNAME="";
-	static  String DB_PASSWORD="";
 	public static  String VK_APP_TOKEN="";
 	public static void main(String[] args) throws IOException, ClientException, ApiException {
 		try (
-				InputStream is1 = AgrApplication.class.getResourceAsStream("/db.txt");
-				BufferedReader reader1 = new BufferedReader(new InputStreamReader(is1));
 				InputStream is2 = AgrApplication.class.getResourceAsStream("/vk.txt");
 		BufferedReader reader2 = new BufferedReader(new InputStreamReader(is2))
 				)
 		{
-			DB_USERNAME=reader1.readLine();
-			DB_PASSWORD=reader1.readLine();
 			VK_APP_TOKEN= reader2.readLine();
 		}
 
