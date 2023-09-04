9ac6f315f436324f951e5f1fafe647117cd6eda0
aidar <myakkaynt228@gmail.com>
2023-09-04 20:24:08 +0300

    testing with mockito

--- src/test/java/com/agregator/agr/service/ProductServiceTests.java
+++ src/test/java/com/agregator/agr/service/ProductServiceTests.java
@@ -1,8 +1,39 @@
 package com.agregator.agr.service;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import com.agregator.agr.dto.ProductDto;
+import com.agregator.agr.models.Product;
+import com.agregator.agr.repositories.ProductRepository;
+import com.agregator.agr.services.impl.ProductServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
-@ExtendWith(MockitoExtension.class)
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ProductServiceTests {
+    @Mock
+    private ProductRepository productRepository;
+    @InjectMocks
+    private ProductServiceImpl productService;
+    @Test
+    public void ProductService_CreateProduct(){
+        Product product1 = Product.builder()
+                .title("Backpack 1")
+                .build();
+        Product product2 = Product.builder()
+                .title("Backpack 2")
+                .build();
+        when(productRepository.searchProducts(Mockito.any(String.class))).thenReturn(List.of(product1,product2));
+
+        List<ProductDto> products = productService.searchProducts("Backpack");
+
+        Assertions.assertThat(products.size()).isEqualTo(2);
+    }
 }
